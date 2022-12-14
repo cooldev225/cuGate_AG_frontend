1547472f3933b42e55f06cc06d905d2321b0e6e3
cooldev225 <goldstar22501@gmail.com>
2022-12-14 09:38:46 +0900

    create tageditor page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { 
+import {
   CugatePage, AuthPage, CumarketPage, CuviewPage,
   CuradioPage, CutrackPage, PlaylistsPage,
   ImpressumPage, PrivacyPolicyPage, OurTeamPage,
   ProfilePage, SubscribePage, MembershipPage,
-  CheckoutPage, DashboardPage
+  CheckoutPage, DashboardPage, TagEditorPage
 } from "./views";
 import AuthRoute from "./utils/AuthRoute";
-import { BrowserRouter as Router, Routes , Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from "./contexts/JWTAuthContext";
 import { DefaultLayout } from "./components";
 import Root from "./components/Root";
@@ -16,29 +16,30 @@ export default function App() {
       <AuthProvider>
         <DefaultLayout>
           <Routes>
-            <Route element={<AuthRoute/>}>
-              <Route path="/profile" element={<ProfilePage/>} />
-              <Route path="/profile/subscribe" element={<SubscribePage/>} />
-              <Route path="/membership" element={<MembershipPage/>} />
-              <Route path="/checkout/:id" element={<CheckoutPage/>} />
-              <Route path="/dashboard" element={<DashboardPage/>} />
+            <Route element={<AuthRoute />}>
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/profile/subscribe" element={<SubscribePage />} />
+              <Route path="/membership" element={<MembershipPage />} />
+              <Route path="/checkout/:id" element={<CheckoutPage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/tageditor" element={<TagEditorPage />} />
             </Route>
             <Route path="/" element={<Root />} />
-            <Route path="/login" element={<AuthPage page={0}/>} />
-            <Route path="/register" element={<AuthPage  page={1}/>} />
-            
-            <Route path="/cugate" element={<CugatePage/>} />
-            <Route path="/cumarket" element={<CumarketPage/>} />
-            <Route path="/cuview" element={<CuviewPage/>} />
-            <Route path="/curadio" element={<CuradioPage/>} />
-            <Route path="/cutrack" element={<CutrackPage/>} />
-            <Route path="/playlists" element={<PlaylistsPage/>} />
-            <Route path="/our-team" element={<OurTeamPage/>} />
-            <Route path="/impressum" element={<ImpressumPage/>} />
-            <Route path="/privacy-policy" element={<PrivacyPolicyPage/>} />
+            <Route path="/login" element={<AuthPage page={0} />} />
+            <Route path="/register" element={<AuthPage page={1} />} />
+
+            <Route path="/cugate" element={<CugatePage />} />
+            <Route path="/cumarket" element={<CumarketPage />} />
+            <Route path="/cuview" element={<CuviewPage />} />
+            <Route path="/curadio" element={<CuradioPage />} />
+            <Route path="/cutrack" element={<CutrackPage />} />
+            <Route path="/playlists" element={<PlaylistsPage />} />
+            <Route path="/our-team" element={<OurTeamPage />} />
+            <Route path="/impressum" element={<ImpressumPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
           </Routes>
         </DefaultLayout>
-        
+
       </AuthProvider>
     </BrowserRouter>
   );
---
+++ src/views/tageditor/contents.ts
@@ -0,0 +1,16 @@
+export const menuList: {
+    key: string;
+    text?: string;
+    icon?: number;
+}[] = [
+        {
+            key: "tracks",
+            text: "Tracks",
+            icon: 0,
+        },
+        {
+            key: "albums",
+            text: "Albums",
+            icon: 0,
+        }
+    ];
